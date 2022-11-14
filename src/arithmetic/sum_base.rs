e4ec933e9abda4d4f219a6c041365dda3b6adad9
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-11-14 22:34:16 +0000

    simplfied code solution, modified tests

--- src/arithmetic/sum_base.rs
+++ src/arithmetic/sum_base.rs
@@ -1,31 +1,11 @@
-pub fn sum_digits(num : i32) -> i32{
-    let num_string : String = format!("{}", num);
-    let mut total : i32 = 0;
-    for digit_str in num_string.split(""){
-        if !digit_str.is_empty(){
-            let digit : i32 = digit_str.parse().unwrap();
-            total += digit;
-        }
-    }
-    total
-}
-
-pub fn get_number_with_new_base(number : i32, base : i32) -> i32{
+pub fn sum_base(number : i32, base : i32) -> i32{
     let mut current_number : i32 = number;
-    let mut str_number : String = String::from("");
+    let mut total : i32 = 0;
     while current_number >= base{
         let current_remainder : i32 = current_number % base;
         current_number /= base;
-        let remainder_as_string : String= format!("{}", current_remainder);
-        str_number = format!("{}{}",remainder_as_string, str_number);
+        total += current_remainder;
     }
-    str_number = format!("{}{}",current_number,str_number);
-    let converted_number : i32 = str_number.parse().unwrap();
-    converted_number
-}
-
-pub fn sum_base(num : i32, base : i32) -> i32{
-    let converted_number : i32 = get_number_with_new_base(num, base);
-    let sumed_digits : i32 = sum_digits(converted_number);
-    sumed_digits
+    total += current_number;
+    total
 }
--- src/arithmetic/sum_base_test.rs
+++ src/arithmetic/sum_base_test.rs
@@ -1,81 +1,60 @@
 #[cfg(test)]
 
 use super::sum_base;
-
 #[test]
-pub fn test_sum_digits(){
-    let num : i32 = 23;
-    let result : i32 = sum_base::sum_digits(num);
-    let mut expected_result : i32 = 5;
-    assert_eq!(result, expected_result);
-    let num2 : i32 = 57;
-    let result2 : i32 = sum_base::sum_digits(num2);
-    expected_result = 12;
-    assert_eq!(result2,expected_result);
-}
-
-#[test]
-pub fn test_get_number_with_new_base(){
+pub fn test_sum_base(){
     let mut num : i32 = 8;
-    let mut result : i32 = sum_base::get_number_with_new_base(num, 3);
-    let mut expected_result : i32 = 22;
+    let mut result : i32 = sum_base::sum_base(num, 3);
+    let mut expected_result : i32 = 4;
     assert_eq!(result,expected_result);
 
     num = 9;
-    result = sum_base::get_number_with_new_base(num, 3);
-    expected_result = 100;
+    result = sum_base::sum_base(num, 3);
+    expected_result = 1;
     assert_eq!(result,expected_result);
 
     num = 10;
-    result = sum_base::get_number_with_new_base(num, 3);
-    expected_result = 101;
+    result = sum_base::sum_base(num, 3);
+    expected_result = 2;
     assert_eq!(result, expected_result);
 
     num = 23;
-    result = sum_base::get_number_with_new_base(num, 3);
-    expected_result = 212;
+    result = sum_base::sum_base(num, 3);
+    expected_result = 5;
     assert_eq!(result, expected_result);
 
     num = 26;
-    result = sum_base::get_number_with_new_base(num, 3);
-    expected_result = 222;
+    result = sum_base::sum_base(num, 3);
+    expected_result = 6;
     assert_eq!(result, expected_result);
 
     num = 27;
-    result = sum_base::get_number_with_new_base(num, 3);
-    expected_result = 1000;
+    result = sum_base::sum_base(num, 3);
+    expected_result = 1;
     assert_eq!(result, expected_result);
 
     num = 6;
-    result = sum_base::get_number_with_new_base(num, 6);
-    expected_result = 10;
+    result = sum_base::sum_base(num, 6);
+    expected_result = 1;
     assert_eq!(result, expected_result);
 
     num = 15;
-    expected_result = 23;
-    result = sum_base::get_number_with_new_base(num, 6);
+    expected_result = 5;
+    result = sum_base::sum_base(num, 6);
     assert_eq!(result, expected_result);
 
     num = 42;
-    expected_result = 110;
-    result = sum_base::get_number_with_new_base(num, 6);
+    expected_result = 2;
+    result = sum_base::sum_base(num, 6);
     assert_eq!(result, expected_result);
 
     num = 10;
-    expected_result = 10;
-    result = sum_base::get_number_with_new_base(num, 10);
+    expected_result = 1;
+    result = sum_base::sum_base(num, 10);
     assert_eq!(result, expected_result);
 
     num = 8;
-    result = sum_base::get_number_with_new_base(num, 2);
-    expected_result = 1000;
-    assert_eq!(result, expected_result);
-}
-
-#[test]
-pub fn test_sum_base(){
-    let num : i32 = 42;
-    let result : i32 = sum_base::sum_base(num, 6);
-    let expected_result : i32 = 2;
+    result = sum_base::sum_base(num, 2);
+    expected_result = 1;
     assert_eq!(result, expected_result);
 }
